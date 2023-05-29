681c839cc30a6d5f3e1d4f00aee6a70cf441eb42
admin <admin@DESKTOP-TJSP1RG>
2023-05-29 13:08:53 +0530

    not extended class

---
+++ new1/src/new1/Myjava.java
@@ -0,0 +1,24 @@
+package new1;
+
+abstract class Code {
+
+	static void info1(String a, String b) {
+		System.out.println("The name is " + (a + b));
+	}
+
+	void info2(int a, int b) {
+		System.out.println("this is non static");
+	}
+
+	abstract void info();
+
+}
+
+
+
+
+public class Myjava {
+	public static void main(String[] args) {
+
+	}
+}
